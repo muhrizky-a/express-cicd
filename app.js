b4add235b7ef86ba041c0069f42134210423ad7d
M. Rizky A <muhrizky1337a@gmail.com>
2022-12-06 15:49:41 +0000

    Change response message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000
 
-app.get('/', (req, res) => res.send("Hello world"));
+app.get('/', (req, res) => res.send("Hello world!"));
 
 app.listen(PORT, () => {
     console.log("Server running on port", PORT);
